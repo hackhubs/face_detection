800472f34c0d40777b9948e75eb9c4d312c1d8ad
hackhubs <abhav.thakur2018@vitstudent.ac.in>
2020-07-28 10:25:42 +0530

    detect opencv

---
+++ src/face.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+import  pickle
+
+face_cascade = cv2.CascadeClassifier('data\haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('data\haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('data\haarcascade_smile.xml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+labels = {"person_name":1}
+with open("label.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels ={v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5,minNeighbors=5)
+    for(x,y,w,h) in faces:
+        # print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+        roi_color = frame[y:y + h, x:x + w]
+        #recognize? deep learned model predict keras tensorflow pytorch sckit learn
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=45: # and conf <=85:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+
+        img_item = "7.png"
+        cv2.imwrite(img_item,roi_color)
+
+        color = (255, 0, 0) #BGR
+        stroke = 2
+        end_cord_x = x+w
+        end_cord_y = y+h
+        cv2.rectangle(frame,(x,y),(end_cord_x, end_cord_y),color,stroke)
+        smile = smile_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in smile:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    #Displaying the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+#when everything done 
+cap.release()
+cv2.destroyWindow()
